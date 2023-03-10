58a65049f30f1c135fa4c3e81c84cb7f3f3385c3
horrormankind666 <yutthaphoom.taw@mahidol.ac.th>
2023-03-10 17:29:23 +0700

    ...

--- sonar-project.js
+++ sonar-project.js
@@ -2,6 +2,6 @@ const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
     serverUrl: process.env.SONAR_SERVER || 'http://localhost:9000',
-    token: process.env.SONAR_TOKEN || 'sqp_642eb038d741c4decefdaf7689965cd1f509a81c',
+    token: process.env.SONAR_TOKEN || 'sqp_d49a610a2c18bc1045718f5aba3c348047dcf4a9',
     options: {}
 }, () => process.exit());
